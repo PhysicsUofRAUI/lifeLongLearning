427fdfa8896e93615778a1aa86df43f3549cd946
Kody Rogers <kodyrogers21@gmail.com>
2020-07-31 16:47:53 -0600

    added a comment

--- set_up.sh
+++ set_up.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Short little script to make setting up my development environment easier.
+# Will have to make sure this script is executable before it can be run.
+# May have to be changed since I haven't used it yet
+#
 sudo apt update
 sudo snap install atom --classic
 sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
