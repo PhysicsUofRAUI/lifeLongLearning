bdd9fbacdbedf98a03ffaf0a004c6defe55837b7
Kody Rogers <kodyrogers21@gmail.com>
2020-10-28 17:14:20 -0600

    transfer tests testing the other views to other_tests.py

--- tests.py
+++ tests.py
@@ -1911,84 +1911,7 @@ class TestingWhileLoggedIn(TestCase):
 
 
 
-    def test_other_database_pages(self):
-        # contact page
-        auth_1 = Author(name='Kidkaidf', email='kodyrogers21@gmail.com', password='pbkdf2:sha256:150000$73fMtgAp$1a1d8be4973cb2676c5f17275c43dc08583c8e450c94a282f9c443d34f72464c')
-        db.session.add(auth_1)
-
-        db.session.commit()
-
-        response = self.client.get('/contact', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-        auth_1 = Author.query.filter_by(name='Kidkaidf').first()
-        with self.app.test_client() as c:
-            with captured_templates(self.app) as templates:
-                r = c.get('/contact')
-                template, context = templates[0]
-                self.assertEqual(context['authors'], [auth_1])
-
-
-
-
-
-        #
-        # Testing the Admin Page
-        #
-        w_cat = WorksheetCategory(name='dundk')
-        db.session.add(w_cat)
-
-        p_cat = PostCategory(name='froots')
-
-        learner = Learner(name='KJsa', email='kodyrogers21@gmail.com', screenname='kod'
-                        , password='pbkdf2:sha256:150000$73fMtgAp$1a1d8be4973cb2676c5f17275c43dc08583c8e450c94a282f9c443d34f72464c')
-
-        learner_1 = Learner(name='Ksa', email='kodyroger21@gmail.com', screenname='kod_1'
-                        , password='pbkdf2:sha256:150000$73fMtgAp$1a1d8be4973cb2676c5f17275c43dc08583c8e450c94a282f9c443d34f72464c')
-
-        learner_2 = Learner(name='KoJsa', email='kodyrogers1@gmail.com', screenname='kod2'
-                        , password='pbkdf2:sha256:150000$73fMtgAp$1a1d8be4973cb2676c5f17275c43dc08583c8e450c94a282f9c443d34f72464c')
-
-        learner_3 = Learner(name='KJdsa', email='kodyrogers2@gmail.com', screenname='kod3'
-                        , password='pbkdf2:sha256:150000$73fMtgAp$1a1d8be4973cb2676c5f17275c43dc08583c8e450c94a282f9c443d34f72464c')
-
-        db.session.add(learner)
-        db.session.add(learner_1)
-        db.session.add(learner_2)
-        db.session.add(learner_3)
-        db.session.commit()
-
-        db.session.add(p_cat)
-        db.session.commit()
-
-        worksheet = Worksheet(pdf_url='tudoloos.pdf', name='tudoloo', author_id=1, author=auth_1, category_id=1, category=w_cat)
-        worksheet_1 = Worksheet(pdf_url='tudoloo.pdf', name='tudolos', author_id=1, author=auth_1, category_id=1, category=w_cat)
-        worksheet_2 = Worksheet(pdf_url='tudolos.pdf', name='tudooos', author_id=1, author=auth_1, category_id=1, category=w_cat)
-        worksheet_3 = Worksheet(pdf_url='tudolos.pdf', name='tudloos', author_id=1, author=auth_1, category_id=1, category=w_cat)
-
-        learner_1.favourites.append(worksheet)
-
-        learner_2.favourites.append(worksheet)
-
-        learner_2.favourites.append(worksheet_2)
-
-        learner_3.favourites.append(worksheet_2)
-
-        db.session.commit()
-
-        response = self.client.get('/admin', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-        with self.app.test_client() as c:
-            with captured_templates(self.app) as templates:
-                login(c, os.getenv('LOGIN_USERNAME'), os.getenv('LOGIN_PASSWORD'))
-                r = c.get('/admin', follow_redirects=False)
-                self.assertEqual(r.status_code, 200)
-                template, context = templates[1]
-                self.assertEqual(context['post_categories'], [p_cat])
-                self.assertEqual(context['worksheet_categories'], [w_cat])
-                self.assertEqual(context['learners'], [learner, learner_1, learner_2, learner_3])
-                logout(c)
+    
 
     def test_learner_delete(self) :
         learner = Learner(name='KJsa', email='kodyrogers21@gmail.com', screenname='kod'
@@ -2074,29 +1997,6 @@ class BasicTests(TestCase):
     # executed after each test
     def tearDown(self):
         pass
-
-    #############################
-    #### testing Other Pages ####
-    #############################
-
-    def test_main_page(self):
-        response = self.client.get('/', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_main_page_home(self):
-        response = self.client.get('/home', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_admin_page(self):
-        response = self.client.get('/admin', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.get('/admin', follow_redirects=False)
-        self.assertEqual(response.status_code, 302)
-
-    def test_building_page(self):
-        response = self.client.get('/building', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
 
     ##############################
     #### testing author pages ####
