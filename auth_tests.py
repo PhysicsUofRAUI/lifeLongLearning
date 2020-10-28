aedd5689345658d9f23da598ce88a35267a1380b
Kody Rogers <kodyrogers21@gmail.com>
2020-10-28 15:28:13 -0600

    fix minor bugs in auth tests file

--- auth_tests.py
+++ auth_tests.py
@@ -1,5 +1,9 @@
-
-
+import unittest
+from flask_testing import TestCase
+from config import TestConfiguration
+from app import create_app as c_app
+import os
+import flask
 
 def login(client, username, password):
     return client.post('/login', data=dict(
@@ -32,19 +36,52 @@ class BasicTests(TestCase):
         pass
 
     def test_login_page(self):
+        #
+        # Test whether the page and form is found
+        #
         response = self.client.get('/login', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
         response = self.client.get('/login', follow_redirects=False)
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
 
         #
         # Now check for to see if it logins in correctly
         #
-        login(self.client, os.getenv('LOGIN_USERNAME'), os.getenv('LOGIN_PASSWORD'))
-        self.assertEqual(flask.session['logged_in'], True)
+        with self.app.test_client() as c:
+            response = c.post('/login', data=dict(
+                username=os.getenv('LOGIN_USERNAME'),
+                password=os.getenv('LOGIN_PASSWORD')
+            ), follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(flask.session.get('logged_in'), True)
+
+            #
+            # Logout and verify the responses
+            #
+            response_1 = c.get('/logout', follow_redirects=True)
+            self.assertEqual(response_1.status_code, 200)
+            self.assertEqual(flask.session['logged_in'], False)
+            
+            
+            #
+            # Try the same things as before, but without following redirects
+            #
+            response = c.post('/login', data=dict(
+                username=os.getenv('LOGIN_USERNAME'),
+                password=os.getenv('LOGIN_PASSWORD')
+            ), follow_redirects=False)
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(flask.session.get('logged_in'), True)
+
+            response_1 = c.get('/logout', follow_redirects=False)
+            self.assertEqual(response_1.status_code, 302)
+            self.assertEqual(flask.session['logged_in'], False)
 
     def test_logout_page(self):
+        #
+        # Testing the logout page while not logged in
+        #
         response = self.client.get('/logout', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
@@ -52,20 +89,6 @@ class BasicTests(TestCase):
         response = self.client.get('/logout', follow_redirects=False)
         self.assertEqual(response.status_code, 302)
 
-        #
-        # Test behaviour while logged in
-        #
-        login(self.client, os.getenv('LOGIN_USERNAME'), os.getenv('LOGIN_PASSWORD'))
 
-        response = self.client.get('/logout', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-        self.assertEqual(flask.session['logged_in'], False)
-
-        # See if it redirected
-        login(self.client, os.getenv('LOGIN_USERNAME'), os.getenv('LOGIN_PASSWORD'))
-
-        response = self.client.get('/logout', follow_redirects=False)
-        self.assertEqual(response.status_code, 302)
-
-        self.assertEqual(flask.session['logged_in'], False)
+if __name__ == "__main__":
+    unittest.main()
